3adfbb39c0035d1da75f2dce58f75338851ab190
Wojciech Pietraszewski <dsw63007@student.ideis.pl>
2026-06-08 01:53:59 +0200

    Add script creating error files

--- skrypt.sh
+++ skrypt.sh
@@ -20,6 +20,27 @@ elif [ "$1" = "--logs" ]; then
 
     echo "Utworzono $count plików"
 
+elif [ "$1" = "--error" ]; then
+    if  [ -n "$2" ]; then
+        count=$2
+    else
+        count=100
+    fi
+
+    for i in $(seq 1 "$count")
+    do
+        dirname="error$i"
+        filename="error${i}.txt"
+        filepath="$dirname/$filename"
+
+        mkdir -p "$dirname"
+
+        echo "Nazwa pliku: $filename" > "$filepath"
+
+    done
+
+    echo "Utworzono $count plików"
+
 elif [ "$1" = "--help" ]; then
     echo "Dostępne opcje:"
     echo "  --date           wyświetla aktualną datę"
